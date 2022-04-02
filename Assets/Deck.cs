b14d9d21bb22f7d8c72f511cf1af6048bb4c5310
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2022-04-02 16:13:56 +0300

    Worked on GameLogic.cs, Deck.cs, Cards.cs
    GameLogic.cs implemented events for StartGame, PauseGame, EndGame functions
    Deck.cs removed functions we won't use, setup for future improvements
    Card.cs made UpdateCard() more up to SOLID

--- Assets/Deck.cs
+++ Assets/Deck.cs
@@ -16,38 +16,13 @@ public class Deck : MonoBehaviour
     }
     public void GenerateDeck()
     {
-        float curZ = startingZ;
-        foreach(GameObject card in cardModels)
-        {
-            //turn this into prefab that is has all of these values instantiated
-            GameObject newCard = new GameObject();
-            newCard.transform.parent = gameObject.transform;
-            Instantiate(card).transform.parent = newCard.transform;
-            newCard.transform.localScale = new Vector3(20f, 20f, 20f);
-            newCard.transform.Rotate(new Vector3(-90f, 0f));
-            newCard.transform.position = new Vector3(newCard.transform.position.x, newCard.transform.position.y, curZ);
-            curZ += distBetweenCards;
-            newCard.AddComponent<Card>();
-            foreach(Transform c in newCard.transform)
-            {
-                newCard.GetComponent<Card>().SetCardModel(c.gameObject);
-            }
-            cards.Add(newCard);
-        }
+        //rework
+        
     }
     public void ShuffleDeck()
     {
-        List<GameObject> tempDeck = new List<GameObject>();
-        CopyTo(cards, tempDeck);
-        float curZ = startingZ;
-        int totalCards = tempDeck.Count;
-        for (int i=0;i<totalCards;i++)
-        {
-            int randomIndex = Random.Range(0, tempDeck.Count);
-            tempDeck[randomIndex].transform.position = new Vector3(tempDeck[randomIndex].transform.position.x, tempDeck[randomIndex].transform.position.y, curZ);
-            curZ += distBetweenCards;
-            tempDeck.RemoveAt(randomIndex);
-        }
+        //rework
+
     }
 
     private void CopyTo(List<GameObject> from, List<GameObject> to)
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,11 +5,44 @@ using System;
 using UnityEngine.Events;
 public class GameManager : MonoBehaviour
 {
+    public UnityEvent OnGameStart;
+    public UnityEvent OnGamePause;
+    public UnityEvent OnGameEnd;
     public UnityEvent OnAwake;
     public UIManager uiManager;
+    GameState gameState;
     private void Awake()
     {
-        OnAwake.Invoke();  
-        
+        OnAwake.Invoke();
+        ChangeState(GameState.MainMenu);
     }
+    public void StartGame(int playerCount)
+    {
+        ChangeState(GameState.Game);
+        OnGameStart.Invoke();
+    }
+    public void PauseGame()
+    {
+        ChangeState(GameState.Pause);
+        OnGamePause.Invoke();
+    }
+    public void EndGame()
+    {
+        ChangeState(GameState.MainMenu);
+        OnGameEnd.Invoke();
+    }
+    private void ChangeState(GameState newState)
+    {
+        gameState = newState;
+    }
+
 }
+public enum GameState
+{
+    MainMenu,
+    Game,
+    Pause
+}
+[Serializable]
+public class GameStateUnityEventArgs: UnityEvent<GameState>
+{ }

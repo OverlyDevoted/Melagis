38a830dcc2392acceb6f5277d0ab85785df063af
rytis-valiukas <rytis.valiukas@tutanota.com>
2022-04-11 15:05:25 +0300

    Inicializuotos PlayTrack ir PlayEffect klasės, sukurti PlayEffect klasės testai

---
+++ Assets/Tests/PlayEffectTest.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.TestTools;
+using System.Collections.Generic;
+
+public class TestPlayTrack {
+
+    public PlayEffect myPlayEffect;
+
+    // Testas nesėkmingas
+
+    [Test]  // Testas MuteEffect() metodui.
+    public void TestMuteEffect()  {
+
+        GameObject name = new GameObject();
+        myPlayEffect= name.AddComponent(typeof (PlayEffect)) as PlayEffect;
+
+        myPlayEffect.MuteEffect();
+        Assert.AreEqual(0.0f, myPlayEffect.effectVolume);
+
+        myPlayEffect.MuteEffect();
+        Assert.AreEqual(1.0f, myPlayEffect.effectVolume);
+    }
+
+    // Testas nesėkmingas
+
+     [Test] // Testas PlayMyEffect() metodui.
+    public void TestPlayMyEffect() {
+        
+        // Sukuriamos nuorodos
+        GameObject gameObject = new GameObject();
+        AudioSource audioSource = gameObject.AddComponent(typeof(AudioSource)) as AudioSource;
+        PlayEffect myAcceptEffect = gameObject.AddComponent(typeof(PlayEffect)) as PlayEffect;
+        
+        // Priskiriamas efektas
+        myAcceptEffect.effectSource = audioSource;  
+        AudioClip myClip = Resources.Load<AudioClip>("Sounds/SFX_Accept");
+
+        // Leidžiama ir tikrinama
+        myAcceptEffect.PlayMyEffect(myClip);
+        Assert.IsTrue(audioSource.isPlaying);
+    }
+}

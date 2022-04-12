0b478be7f82d5dea1fd5bee9e086419da9f9ab6a
Luuuuukas <75544324+Luuuuukas@users.noreply.github.com>
2022-04-12 16:51:52 +0300

    Add files via upload

--- Assets/Tests/StatisticsTest2.cs
+++ Assets/Tests/StatisticsTest2.cs
@@ -8,21 +8,16 @@ using System;
 
 public class StatisticsTest2 : MonoBehaviour
 {
-    // Testas, palygina 2 failus 
     [Test]
     public void TestCompareTwoDataFiles()
     {
         GameObject testObject = new GameObject();
         Statisticsscript statistics = testObject.AddComponent<Statisticsscript>();
-        //Perskaitomas pirmas failas
         string line = "";
         try
         {
-            //Pass the file path and file name to the StreamReader constructor
             StreamReader sr = new StreamReader(@"C:\Users\LENOVO\Desktop\Darbalaukis\Universiteto\Trecias kursas\Pavasario Semestras\Programu kurimo procesas\Melagis-develop (1)\Melagis-develop\Assets\Statistics\testfile1.txt");
-            //Read the first line of text
             line = sr.ReadLine();
-            //Continue to read until you reach end of file
             while (line != null)
             {
                 Console.WriteLine(line);
@@ -42,11 +37,3 @@ public class StatisticsTest2 : MonoBehaviour
         Assert.AreEqual("testuojami duomenys", statistics.readtext());
     }
 }
-
-// BDD - Behaviour driven development buvo pritaikytas kuriant ðá testà
-// TDD - Test Driven development buvo pritaikytas kuriant ðá koda -> parodyti test runnery su ivykdytais testais
-
-
-// TDD Toks programinës technikos testavimo bûdas kurio metu testo atvejai yra kuriami,
-// daþnai automatizuojami ir tada programinë árangà yra kartu kuriama inkrementaliai
-// siekiant pereiti teiso scenarijus be klaidø. //Testo atvejai kuriami kartu su kodu.//
